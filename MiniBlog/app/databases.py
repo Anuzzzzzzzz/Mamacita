6df6baf8404797ff226934facb8b38708388f3e3
Anuj Paudel <danuzz@Anujs-MacBook-Air.local>
2026-01-12 16:33:10 +0545

    tried learning db

---
+++ MiniBlog/app/databases.py
@@ -0,0 +1,21 @@
+# app/database.py
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+import os
+from sqlalchemy import Column, Integer, String
+
+
+# Path for SQLite
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DB_PATH = os.path.join(BASE_DIR, "blog.db")
+
+SQLALCHEMY_DATABASE_URL = f"sqlite:///{DB_PATH}"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={"check_same_thread":False}
+
+)
+
+SessionLocal= sessionmaker(bind=engine, autoflush=False, autocommit= False)
+Base= declarative_base()
