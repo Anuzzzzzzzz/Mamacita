2ab40375642b3e29c33ae9ab8044c182c55bfa3b
Anuj Paudel <danuzz@Anujs-MacBook-Air.local>
2026-01-22 15:39:09 +0545

    pandas

--- FastApi/databases.py
+++ FastApi/databases.py
@@ -1,22 +1,1 @@
-# File: FastApi/databases.py
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-
-# Using SQLite for instant setup. 
-# (To use Postgres later, just change this one line).
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-Base = declarative_base()
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+# File: FastApi/databases.pyfrom sqlalchemy import create_enginefrom sqlalchemy.ext.declarative import declarative_basefrom sqlalchemy.orm import sessionmaker# Using SQLite for instant setup. # (To use Postgres later, just change this one line).SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"engine = create_engine(    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)Base = declarative_base()def get_db():    db = SessionLocal()    try:        yield db    finally:        db.close()
